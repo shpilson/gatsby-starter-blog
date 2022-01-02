df645b9061287d19a49886b1d02e1a325810af99
shpilson <kovalenko.jora@gmail.com>
2022-01-02 14:02:40 +0300

    updating

--- gatsby-config.js
+++ gatsby-config.js
@@ -5,7 +5,7 @@ module.exports = {
       name: `Kowalenko`,
       summary: `– про всё, что кажется интересным, простым языком.`,
     },
-    description: `Авторский блог sГеоргия Коваленко.`,
+    description: `Авторский блог Георгия Коваленко.`,
     siteUrl: `https://blog.kowalenko.net/`,
     social: {
       twitter: `shpilzen`,
