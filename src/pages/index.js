61e085b241e3692b48f4aa7cb806db4e51ed1d10
shpilson <kovalenko.jora@gmail.com>
2022-01-08 23:47:19 +0300

    gatsby

--- src/pages/index.js
+++ src/pages/index.js
@@ -17,7 +17,7 @@ const BlogIndex = ({ data, location }) => {
         <p>
           No blog posts found. Add markdown posts to "content/blog" (or the
           directory you specified for the "gatsby-source-filesystem" plugin in
-          gatsby-config.js).
+          gatsby-config.js).ы
         </p>
       </Layout>
     )
